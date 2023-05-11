9b290ff74e38a0dae3ddd232cb441d94b11fa7df
Rambhu Mukhia <Rambhu.Mukhia@euromonitor.com>
2023-05-11 15:24:51 +0530

    added conditional update example

--- signals-angular16/src/app/app.component.ts
+++ signals-angular16/src/app/app.component.ts
@@ -7,21 +7,33 @@ import { Component, computed, effect, signal } from '@angular/core';
 })
 export class AppComponent {
   title = 'signals-angular16';
-  count=signal(0)
+  count=signal(0);
+  showCount=signal(false);
 
-  double =computed(()=>this.count()*2)
+  double =computed(()=>this.count()*2);
 
-  countType=computed(()=>this.count() % 2===0 ? 'even':'odd')
+  countType=computed(()=>this.count() % 2===0 ? 'even':'odd');
+
+  conditionalCount=computed(()=>{
+    if(this.showCount()){
+      return `The count is ${this.count()}`
+    }else{
+      return null
+    }
+  })
 
   constructor(){
     effect(()=>{
       console.log('count changed to ',this.count())
       console.log(this.count(),'is', this.countType())
+      console.log('showcount',this.showCount())
     })
   }
 
   Increase(){
     this.count.update(c=>c+1)
+    this.showCount.set(true)
+
   }
 
   Decrease(){
@@ -29,6 +41,7 @@ export class AppComponent {
   }
   reset (){
     this.count.set(0)
+    this.showCount.set(false)
   }
 
 }
